e409e941ae69a265cf5a5874f728d421ac818b43
unknown <md.tanvirhasananik@gmail.com>
2020-02-02 03:29:23 +0600

    timedone

---
+++ database/migrations/2020_01_30_042531_create_time_slots_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTimeSlotsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('time_slots', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->dateTime('startSlotTime');
+            $table->dateTime('endSlotTime');
+            $table->string('day');
+            $table->dateTime('date')->nullable();
+            $table->boolean('status')->default('0');
+            $table->integer('user_id')->unsigned()->nullable();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade'); 
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('time_slots');
+    }
+}
